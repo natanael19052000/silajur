bd3f8207e0d6ea413030c62c3a14801791721f4f
natanael19052000 <nael1952000@gmail.com>
2021-03-15 15:46:39 +0700

    Update modal.php

--- application/views/modal.php
+++ application/views/modal.php
@@ -198,11 +198,81 @@
             </div>
             <div class="modal-body">
             Isikan Data Berikut
-            </div>
+            Pemberian entertaiment dan sejenisnya
+            <form>
+            <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Nomor</label>
+                <div class="col-sm-7">
+                    <input type="text" class="form-control" name="Nomor" placeholder="Masukkan Nomor">
+                </div>
+              </div>
+            <div class="form-group row">
+              <label class="col-sm-3 col-form-label">Tanggal</label>
+              <div class="col-sm-7">
+                  <input type="date" class="form-control" name="tgl" placeholder="Pilih Tanggal">
+                </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-3 col-form-label">Tempat</label>
+                    <div class="col-sm-7">
+                        <textarea type="text" rows="2" name="tmpt" class="form-control"></textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-3 col-form-label">Alamat</label>
+                    <div class="col-sm-7">
+                        <textarea type="text" rows="5" name="almt" class="form-control"></textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-3 col-form-label">Jenis</label>
+                    <div class="col-sm-7">
+                        <textarea type="text" rows="5" name="jenis" class="form-control"></textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-3 col-form-label">Jumlah</label>
+                    <div class="col-sm-7">
+                        <input type="text" class="form-control" name="jumlah" placeholder="Masukkan Jumlah">
+                    </div>
+                  </div>
+                  Relasi Usaha yang diberikan entertaiment dan sejenisnya
+                  <div class="form-group row">
+                      <label class="col-sm-3 col-form-label">Nama</label>
+                      <div class="col-sm-7">
+                          <textarea type="text" rows="1" name="nm_usaha" class="form-control"></textarea>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label class="col-sm-3 col-form-label">Posisi</label>
+                      <div class="col-sm-7">
+                          <textarea type="text" rows="1" name="posisi" class="form-control"></textarea>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label class="col-sm-3 col-form-label">Nama Perusahaan</label>
+                      <div class="col-sm-7">
+                          <textarea type="text" rows="1" name="nm_perusahaan" class="form-control"></textarea>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label class="col-sm-3 col-form-label">Jenis Usaha</label>
+                      <div class="col-sm-7">
+                          <textarea type="text" rows="2" name="jn_usaha" class="form-control"></textarea>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label class="col-sm-3 col-form-label">Keterangan</label>
+                      <div class="col-sm-7">
+                          <textarea type="text" rows="5" name="ket" class="form-control"></textarea>
+                      </div>
+                  </div>
             <div class="modal-footer">
                 <button class="btn btn-success">submit</button>
             </div>
             </form>
-        </div>
-    </div>
+            </div>
+            </div>
+            </div>
+
 </div>
